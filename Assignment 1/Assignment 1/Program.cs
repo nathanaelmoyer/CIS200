6415ffd4423935631d8dbf255614414f0e44fef6
Noah Moyer <35582108+NoahMoyer@users.noreply.github.com>
2018-01-27 21:55:27 -0500

    updates

--- Assignment 1/Assignment 1/Program.cs
+++ Assignment 1/Assignment 1/Program.cs
@@ -8,8 +8,35 @@ namespace Assignment_1
 {
     class Program
     {
+        static double totalEarnings = 0;
+        static double taxTemp;
+        static double totalTax, taxAt10, taxAt15, taxAt25, taxAt33, taxAt35, taxAt396;
+
         static void Main(string[] args)
         {
+            string entryAsString;
+            double entryAsDouble;
+
+            Console.WriteLine("Welcome to your federal income tax calculator!");
+            Console.WriteLine("Please enter the total taxable income for each W2.");
+            do {
+               entryAsString = Console.ReadLine();
+               entryAsDouble = Convert.ToDouble(entryAsString);
+                if (entryAsDouble != -1){
+                    totalEarnings += entryAsDouble;
+                }
+            } while (entryAsDouble != -1);
+
+            //using single bracket
+            //first tax bracket
+            taxTemp = totalEarnings;
+            if (totalEarnings > 9325){
+                taxTemp -= 9325;
+
+            }
+            else if(totalEarnings <= 9325){
+
+            }
         }
     }
 }
