7595ec76a9928dd26ffbf930c3063b50a40413ed
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-02-12 14:19:02 -0500

    Initial lab 4

---
+++ labs/lab4/program template/program template/Source.cpp
@@ -0,0 +1,71 @@
+//Program Name
+//Nathanael Moyer
+//Date
+
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <assert.h>
+
+using namespace std;
+
+//####################################################################
+
+#include "VECTOR_H.h"
+
+//####################################################################
+
+void line()
+{
+
+	cout << endl << "--------------------------------------------------------------------------------" << endl;
+
+}
+
+
+int main()
+{
+	line();
+	cout << "Welcome to my program!";
+	line();
+	//####################################################################
+
+	//REQUIRED CODE
+
+	Vector a, b(3), c(3);
+
+	a.print();			//outputs[]
+	b.print();			//outputs[0 0 0]
+	c.set(0, -1);		//outputs error message
+	c.set(1, 0);
+	c.set(2, 1);
+	c.set(3, 2);
+	c.set(4, 3);		//outputs error message
+	c.print();			//outputs[1 2 3]
+
+	Vector d(c);
+	d.print();			//outputs[1 2 3]
+
+	d.set(0, 1);
+	d.print();			//outputs [1 0 3]
+	c.print();			//outputs [1 2 3] proves deep copy
+
+	//My code after this point
+
+
+
+	
+
+
+	//####################################################################
+	line();
+	cout << "Thank you for running the program written by Nathan!";
+	line();
+
+
+	return 0;
+}
+
---
+++ labs/lab4/program template/program template/VECTOR_H.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#ifndef VECTOR_H
+#define VECTOR_H
+class Vector
+{
+public:
+	Vector();		//Defualt constructor
+
+	Vector(int s)
+	{
+
+	}
+					//makes size = s
+					//allocates s space
+					// e.g. entries = new int[size]
+					//makes all entries 0
+
+	Vector(const Vector & other)
+	{
+
+	}
+									//copy constructor
+									//makes a deep copy
+
+	~Vector();		//default deconstructor
+
+	void print()		//prints out the vector
+	{
+
+	};
+	void set(int val, int pos)		//if 0<=pos<size
+	{
+
+	};
+									//stores val at pos in entries
+									//otherwise
+									//error message
+
+private:
+	int size;				//sets the # of elements used
+	int *entries;			//point to array of integers with size entries
+							//e.g. entries = new int[size]
+};
+
+
+#endif
