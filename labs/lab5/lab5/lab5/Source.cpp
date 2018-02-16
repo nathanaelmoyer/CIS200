720dd48f7a2275b090495dfaca5a6f51f0e5bdcb
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-02-16 01:11:47 -0500

    finished lab5prog1

--- labs/lab5/lab5/lab5/Source.cpp
+++ labs/lab5/lab5/lab5/Source.cpp
@@ -21,12 +21,12 @@ void line()
 
 }
 
-int recursiveLinearSearch(int count, int array[], int key, int size, bool methodStatus)
+int recursiveLinearSearch(int &count, int array[], int key, int size, bool methodStatus)
 {
+	++count;
 	int first = 0;
 	int last = size;
 	int mid = (first + last) / 2;
-	
 	
 	if (key < 0)
 	{
@@ -38,15 +38,11 @@ int recursiveLinearSearch(int count, int array[], int key, int size, bool method
 	}
 	if (key < mid) //check values less than the middle value
 	{
-		
 		recursiveLinearSearch(count, array, key, size - 1, methodStatus);
-		
 	}
 	else if (key > mid)	//check values greater than the middle value
 	{
-		
 		recursiveLinearSearch(count, array, key, size + 1, methodStatus);
-		
 	}
 	else if (key == mid)	//check middle value
 	{
@@ -64,32 +60,46 @@ int main()
 	line();
 	//####################################################################
 
+	char response = 'a';
 	int count = 0;
-	int size = 2776;
+	int size = 2500;
 	bool methodStatus = false;
 	int key = 0;
 
-	int array[2776];
+	int array[2500];
 	for (int i = 0; i < size; i++)	//initialize array to sequential numbers starting from 0
 	{
 		array[i] = i;
 	}
-
-	cout << "Enter key value to search for: ";
-	cin >> key;
-	
-	methodStatus = recursiveLinearSearch(count, array, key, size, methodStatus);
-
-	if (methodStatus == 1)
+	while (response != 'n' && response != 'N')
 	{
-		cout << "True" << endl;
+		cout << "Enter key value to search for: ";
+		cin >> key;
+
+		methodStatus = recursiveLinearSearch(count, array, key, size, methodStatus);
+
+		if (methodStatus == 1)	//check if key value is within array
+		{
+			cout << key << " is within the current array" << endl;
+		}
+		else
+		{
+			cout << key << " is not within the current array" << endl;
+		}
+		cout << "Recursive function called " << count << " times" << endl;
+
+		line();
+
+		cout << "Would you like to enter another value to search for" << endl;		//continue loop
+		cin >> response;
+		while (response != 'n' && response != 'N' && response != 'y' && response != 'Y')
+		{
+			cout << "Please enter a valid response" << endl;
+			cin >> response;
+		}
+
+		line();
 	}
-	else
-	{
-		cout << "False" << endl;
-	}
-	cout << count << endl;
-	
 	//#####################################################################
 	line();
 	cout << "Thank you for running the program written by Nathan!";
