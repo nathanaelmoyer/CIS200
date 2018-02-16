0f03fda1c8348d37c9cb6ac02ab949085395e41f
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-02-16 00:08:26 -0500

    I think i did this right lol

--- labs/lab5/lab5/lab5/Source.cpp
+++ labs/lab5/lab5/lab5/Source.cpp
@@ -21,14 +21,38 @@ void line()
 
 }
 
-int recursiveLinearSearch(int array[], int key, int size, bool methodStatus)
+int recursiveLinearSearch(int count, int array[], int key, int size, bool methodStatus)
 {
 	int first = 0;
-	int last = size - 1;
+	int last = size;
 	int mid = (first + last) / 2;
 	
 	
-	return 0;
+	if (key < 0)
+	{
+		return 0;
+	}
+	if (key > size)
+	{
+		return 0;
+	}
+	if (key < mid) //check values less than the middle value
+	{
+		
+		recursiveLinearSearch(count, array, key, size - 1, methodStatus);
+		
+	}
+	else if (key > mid)	//check values greater than the middle value
+	{
+		
+		recursiveLinearSearch(count, array, key, size + 1, methodStatus);
+		
+	}
+	else if (key == mid)	//check middle value
+	{
+		return 1;
+	}
+	
 	
 }
 
@@ -40,20 +64,31 @@ int main()
 	line();
 	//####################################################################
 
-	
-	int size = 10;
+	int count = 0;
+	int size = 2776;
 	bool methodStatus = false;
 	int key = 0;
-	int array[10];
-	for (int i = 0; i < size; i++)
+
+	int array[2776];
+	for (int i = 0; i < size; i++)	//initialize array to sequential numbers starting from 0
 	{
 		array[i] = i;
 	}
 
 	cout << "Enter key value to search for: ";
 	cin >> key;
+	
+	methodStatus = recursiveLinearSearch(count, array, key, size, methodStatus);
 
-	recursiveLinearSearch(array, key, size, methodStatus);
+	if (methodStatus == 1)
+	{
+		cout << "True" << endl;
+	}
+	else
+	{
+		cout << "False" << endl;
+	}
+	cout << count << endl;
 	
 	//#####################################################################
 	line();
