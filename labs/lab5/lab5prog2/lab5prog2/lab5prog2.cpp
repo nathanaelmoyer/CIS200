f29d0aa1a2621d4470d10ed12a0b931ee116727a
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-02-20 17:03:39 -0500

    initial lab5prog2

---
+++ labs/lab5/lab5prog2/lab5prog2/lab5prog2.cpp
@@ -0,0 +1,47 @@
+//Program Name
+//Nathanael Moyer
+//Date
+
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <assert.h>
+
+using namespace std;
+
+
+
+void line()
+{
+
+	cout << endl << "--------------------------------------------------------------------------------" << endl;
+
+}
+
+
+
+
+int main()
+{
+	line();
+	cout << "Welcome to my program!";
+	line();
+	//####################################################################
+
+
+
+
+
+
+	//#####################################################################
+	line();
+	cout << "Thank you for running the program written by Nathan!";
+	line();
+
+
+	return 0;
+}
+
