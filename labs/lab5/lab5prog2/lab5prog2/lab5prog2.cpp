bb7a19b82d1f636778f9e3ffafd7efa79f3c370a
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-02-21 13:29:05 -0500

    finished factorial

--- labs/lab5/lab5prog2/lab5prog2/lab5prog2.cpp
+++ labs/lab5/lab5prog2/lab5prog2/lab5prog2.cpp
@@ -22,6 +22,16 @@ void line()
 }
 
 
+int factorial(int value, bool methodStatus)
+{
+	if (value == 0 || value == 1)
+	{
+		return 1;
+	}
+	return value * factorial(value - 1, methodStatus);
+	
+}
+
 
 
 int main()
@@ -31,11 +41,36 @@ int main()
 	line();
 	//####################################################################
 
+	int value = 0;
+	bool methodStatus = 0;
+	char response = 'a';
 
+	while (response != 'n' && response != 'N')	//loop until user enters n or N
+	{
+		cout << "Enter an integer to solve its factorial: " << endl;
+		cin >> value;
 
+		while (value < 0)	//check user input if value is negeative
+		{
+			cout << "Invalid input. Value must be greater than 0" << endl;
+			cin >> value;
+		}
 
+		cout << "Answer: " << factorial(value, methodStatus);
 
+		line();
 
+		cout << "Would you like to solve another factorial? (y/n)" << endl;
+		cin >> response;
+		while (response != 'n' && response != 'N' && response != 'y' && response != 'Y')
+		{
+			cout << "Please enter a valid response. (y/n)" << endl;
+			cin >> response;
+		}
+
+		line();
+
+	}
 	//#####################################################################
 	line();
 	cout << "Thank you for running the program written by Nathan!";
