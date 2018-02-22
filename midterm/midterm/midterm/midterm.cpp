08b8b22bee45c7279026094795b8f214df8f083b
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-02-22 11:04:37 -0500

    midterm

---
+++ midterm/midterm/midterm/midterm.cpp
@@ -0,0 +1,47 @@
+//Midterm
+//Nathanael Moyer
+//Date: 2-22-2018
+
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <assert.h>
+
+using namespace std;
+
+
+
+void line()
+{
+
+	cout << endl << "--------------------------------------------------------------------------------" << endl;
+
+}
+
+
+
+
+int main()
+{
+	line();
+	cout << "Welcome to my program!";
+	line();
+	//####################################################################
+
+
+
+
+
+
+	//#####################################################################
+	line();
+	cout << "Thank you for running the program written by Nathan!";
+	line();
+
+
+	return 0;
+}
+
