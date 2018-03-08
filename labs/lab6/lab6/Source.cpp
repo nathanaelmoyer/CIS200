d6cc904c2b7bd17db24116b3b499299cd45dfa2f
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-03-08 11:18:24 -0500

    created lab6

---
+++ labs/lab6/lab6/Source.cpp
@@ -0,0 +1,47 @@
+//Program Name
+//Nathanael Moyer
+//Date
+
+
+#include <iostream>//cin and cout operators
+#include <string>//string class
+#include <iomanip>//manipulators for output
+#include <fstream>//file stream for files
+#include <cstdlib>//c math operators and many other basic funcitons
+#include <assert.h>//assert header file for the assert operator
+
+using namespace std;
+
+
+
+void line()
+{
+
+	cout << endl << "--------------------------------------------------------------------------------" << endl;
+
+}
+
+
+
+
+int main()
+{
+	line();
+	cout << "Welcome to my program!";
+	line();
+	//####################################################################
+
+
+
+
+
+
+	//#####################################################################
+	line();
+	cout << "Thank you for running the program written by Nathan!";
+	line();
+
+
+	return 0;
+}
+
