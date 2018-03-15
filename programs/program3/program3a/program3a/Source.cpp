2dd09e2ec2e68b9567c693750caa6adf213b0ae4
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-03-15 00:17:06 -0400

    testing prog3a

--- programs/program3/program3a/program3a/Source.cpp
+++ programs/program3/program3a/program3a/Source.cpp
@@ -39,9 +39,53 @@ int main()
 	line();
 	//####################################################################
 
-	
+	Vehicle a;
+	Car b;
+	Truck c;
 
+	//Test vehicle class
+	cout << "Initial vehicle values: " << endl;
+	cout << "Age: " << a.getAge() << " years" <<  endl;
+	cout << "Price: $" << a.getPrice() << endl << endl;
 
+	a.setAge(10);
+	a.setPrice(4000.00);
+	cout << "Updated vehicle values: " << endl;
+	cout << "Age: " << a.getAge() << " years" << endl;
+	cout << "Price: $" << a.getPrice() << endl << endl;
+	line();
+
+	//Test car class
+	cout << "Initial car values: " << endl;
+	cout << "Age: " << b.getAge() << " years" << endl;
+	cout << "Price: $" << b.getPrice() << endl;
+	cout << "Race car status: ";
+	if (b.getRaceCarStatus() == 1)
+	{
+		cout << "true" << endl;
+	}
+	else
+	{
+		cout << "false" << endl;
+	}
+	cout << endl;
+
+	b.setAge(5);
+	b.setPrice(10000.00);
+	b.setRaceCarStatus(true);
+	cout << "Updated car values: " << endl;
+	cout << "Age: " << b.getAge() << " years" << endl;
+	cout << "Price: $" << b.getPrice() << endl;
+	cout << "Race car status: ";
+	if (b.getRaceCarStatus() == 1)
+	{
+		cout << "true" << endl;
+	}
+	else
+	{
+		cout << "false" << endl;
+	}
+	cout << endl;
 
 
 	//#####################################################################
