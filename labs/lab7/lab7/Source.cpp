d15ce646ee4f73c67ac0492777b178be9c81461d
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-03-15 17:20:32 -0400

    testing linklists

--- labs/lab7/lab7/Source.cpp
+++ labs/lab7/lab7/Source.cpp
@@ -21,7 +21,11 @@ void line()
 
 }
 
-
+struct node
+{
+	int number;
+	node * next;
+};
 
 
 int main()
@@ -30,9 +34,78 @@ int main()
 	cout << "Welcome to my program!";
 	line();
 	//####################################################################
+	
+	node * head = NULL;
+	node * previous = NULL;
+	node * location = NULL;
+	node * temp = NULL;
+		
+	
+
+	for (int i = 5; i < 25; i = i + 5)
+	{
+	if (head == NULL)
+	{
+	head = new node;
+	location = head;
+	location->number = 5;
+	location->next = NULL;
+	}
+	else
+	{
+	location->next = new node;
+	location = location->next;
+	location->number = i;
+	location->next = NULL;
+
+	}
+	}
+
+	location = head;
+	while (location != NULL)
+	{
+	cout << location->number << endl;
+	location = location->next;
+	}
 
 
 
+
+	int inputNum;
+	cout << "Insert number: ";
+	cin >> inputNum;
+
+	if (head == NULL)
+	{
+		location = new node;
+		location->number = inputNum;
+		location->next = head;
+		head = location;
+	}
+	else
+	{
+		previous = head;
+		location = head->next;
+		while (location != NULL && location->number < inputNum)
+		{
+			previous = location;
+			location = location->next;
+		}
+		temp = new node;
+		temp->number = inputNum;
+		temp->next = location;
+		previous->next = temp;
+	}
+
+	location = head;
+
+	while (location != NULL)
+	{
+		cout << location->number << endl;
+		location = location->next;
+	}
+
+	
 
 
 
