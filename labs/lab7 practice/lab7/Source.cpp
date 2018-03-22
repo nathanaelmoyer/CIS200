888ab2eba41b2874ba51f1e612d8659ba3f60ffb
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-03-22 01:47:13 -0400

    this is bull shit

--- labs/lab7 practice/lab7/Source.cpp
+++ labs/lab7 practice/lab7/Source.cpp
@@ -96,6 +96,7 @@ int main()
 		temp->next = location;
 		previous->next = temp;
 	}
+	
 
 	location = head;
 
--- labs/lab7/lab7/Source.cpp
+++ labs/lab7/lab7/Source.cpp
@@ -22,53 +22,41 @@ void line()
 
 }
 
-void charConvert(string strInput, char chInput[])	//convert string to char array
+void charCopy(string strInput, char chInput[])	//convert string to char array
 {
 	for (int i = 0; i < strInput.size(); i++)
 	{
-		while (strInput[i] == strInput[i-1])
-		{
-
-		}
 		chInput[i] = strInput[i];
 	}
 }
 
 struct sortedListNode
 {
-	char letter;
+	string letter;
 	int occurrences;
 	sortedListNode * next;
 };
 
 sortedListNode * fromString(string &input)
 {
-	sortedListNode * temp = NULL;
-
+	sortedListNode * temp;
+	temp->letter = input;
+	sort(temp->letter.begin(), temp->letter.end());
 	
 	
 	return temp;
 }
 
-void printList(sortedListNode input)
+void printList(sortedListNode * input)
 {
-	for (int i = 0; i < input.occurrences; i++)
+	while (input != NULL)
 	{
-		if (true)
-		{
+		cout << input->letter << endl;
+		input = input->next;
 
-		}
 	}
 }
 
-char copyStringToArray(string stringInput, char charArray[])
-{
-	for (int i = 0; i < stringInput.size(); i++)
-	{
-
-	}
-	return 0;
-}
 
 int main()
 {
@@ -86,35 +74,30 @@ int main()
 
 	cout << "Enter first word: ";
 	cin >> word1;
+	cout << "Enter second word: ";
+	cin >> word2;
 	
-	char charArray[50];
-	charConvert(word1, charArray);
+	char charArray[255];
+	charCopy(word1, charArray);
 
-	for (int i = 0; i < word1.size(); i++)
+	/*for (int i = 0; i < word1.size(); i++)
 	{
 		cout << charArray[i];
 	}
 	cout << endl;
+*/
 
 
 
 
+	
 
-	//cout << "Enter first word: ";
-	//cin >> word1;
-	//cout << "Enter second word: ";
-	//cin >> word2;
+	list1 = fromString(word1);
+	cout << "Letter list from word one: " << endl;
+	cout << list1->letter << endl;
+	//printList(list1);
 
-	//list1 = fromString(word1);
-	//cout << "Letter list from word one: " << endl;
-	////printList(list1);
-
-	//while (list1 != NULL)
-	//{
-	//	cout << list1->letter << endl;
-	//	list1 = list1->next;
-
-	//}
+	
 	
 
 	//#####################################################################
