01d471fac67196b7bdcb86c9ceab6e7339b5082f
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-03-27 13:59:32 -0400

    fucking file wont fuching open for no god damn reason

--- labs/lab8/lab8/Source.cpp
+++ labs/lab8/lab8/Source.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-
+#define inFile "integer.dat"
 
 void line()
 {
@@ -30,85 +30,43 @@ struct node
 
 int main()
 {
+	fstream ints;
+	ints.open(inFile);	//open file
+	line();
+	if (ints.fail())	//check if file is open
+	{
+		cout << "***ERROR: Failed to open file integer.dat***" << endl;
+		return 0;
+	}
+	else
+	{
+		cout << "integer.dat opened" << endl;
+	}
+	line();
+
+
+	//####################################################################
 	line();
 	cout << "Welcome to my program!";
 	line();
 	//####################################################################
 
 	node * head = NULL;
-	node * previous = NULL;
+	node * forward = NULL;
+	node * backward = NULL;
 	node * location = NULL;
 	node * temp = NULL;
 
-
-
-	for (int i = 5; i < 25; i = i + 5)
-	{
-		if (head == NULL)
-		{
-			head = new node;
-			location = head;
-			location->number = 5;
-			location->next = NULL;
-		}
-		else
-		{
-			location->next = new node;
-			location = location->next;
-			location->number = i;
-			location->next = NULL;
-
-		}
-	}
-
-	location = head;
-	while (location != NULL)
-	{
-		cout << location->number << endl;
-		location = location->next;
-	}
+	node * forwardEven = NULL;
+	node * backwardEven = NULL;
 
 
 
-
-	int inputNum;
-	cout << "Insert number: ";
-	cin >> inputNum;
-
-	if (head == NULL)
-	{
-		location = new node;
-		location->number = inputNum;
-		location->next = head;
-		head = location;
-	}
-	else
-	{
-		previous = head;
-		location = head->next;
-		while (location != NULL && location->number < inputNum)
-		{
-			previous = location;
-			location = location->next;
-		}
-		temp = new node;
-		temp->number = inputNum;
-		temp->next = location;
-		previous->next = temp;
-	}
-
-
-	location = head;
-
-	while (location != NULL)
-	{
-		cout << location->number << endl;
-		location = location->next;
-	}
+	
 
 
 
-
+	ints.close();
 
 	//#####################################################################
 	line();
