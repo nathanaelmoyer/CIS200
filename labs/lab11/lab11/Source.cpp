c76fc52dacf3369fde6428ee204a1a48f42b159c
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2018-04-17 14:44:08 -0400

    linked lists are fucking stupid

--- labs/lab11/lab11/Source.cpp
+++ labs/lab11/lab11/Source.cpp
@@ -28,27 +28,29 @@ struct recordArr
 	double cost;
 };
 
-void addRecord(recordArr arrSorted[], recordArr arrUnsorted[], listNode * listSorted, listNode * listUnsorted, fstream &ons)
+void addRecord(recordArr arrSorted[], recordArr arrUnsorted[], listNode  headListSorted, listNode * headListUnsorted, fstream &ons)
 {
 	listNode * location = NULL;
 	listNode * temp = NULL;
 	listNode * previous = NULL;
 
 	string stringInput = "a";
+	char toolName[21];
 	int intInput = 0;
-	double input = 0.0;
+	int userNum = 0;
+	double doubleInput = 0.0;
 
 	cout << "Enter a record number: ";
-	cin >> intInput;
+	cin >> userNum;
 
-	while (intInput < 1 || intInput > 100)	//check user input for record number
+	while (userNum < 1 || userNum > 100)	//check user input for record number
 	{
 		cout << "Invalid input. Record number must be 1-100" << endl;
-		cin >> intInput;
+		cin >> userNum;
 	}
 
 	int comparisons = 0;
-	while (location != NULL && location->recordNum != intInput)
+	while (location != NULL && location->recordNum != userNum)
 	{
 		location = location->next;
 		comparisons++;
@@ -63,15 +65,88 @@ void addRecord(recordArr arrSorted[], recordArr arrUnsorted[], listNode * listSo
 			cout << "Name is too long. Must be 20 or less characters." << endl;
 			cin >> stringInput;
 		}
-		char toolName[21];
+		
 		strcpy_s(toolName, stringInput.c_str());	//copy stringInput to toolName array
 
 		cout << "Quantity: ";
 		cin >> intInput;
-		
-		
-		
+		while (intInput < 0)	//check value of quantity is positive
+		{
+			cout << "Quantity must be greater than 0." << endl;
+			cin >> intInput;
+		}
+
+		cout << "Cost: ";
+		cin >> doubleInput;
+		while (doubleInput < 0)	//check value of cost is positive
+		{
+			cout << "Cost must be greater than 0." << endl;
+			cin >> doubleInput;
+		}
 	}
+
+	//adding to sorted linked list
+	comparisons = 0;
+	if (headListSorted->recordNum > userNum)
+	{
+		temp = new listNode;
+		temp->recordNum = userNum;
+		for (int i = 0; toolName[i] == NULL; i++)
+		{
+			temp->toolName[i] = toolName[i];
+		}
+		temp->quantity = intInput;
+		temp->cost = doubleInput;
+		temp->next = headListSorted;
+		headListSorted = temp;
+		comparisons++;
+	}
+	else
+	{
+		previous = headListSorted;
+		location = headListSorted->next;
+		while (location != NULL && location->recordNum < userNum)
+		{
+			previous = location;
+			location = location->next;
+			comparisons++;
+		}
+
+		if (location == NULL)
+		{
+			temp = new listNode;
+			temp->recordNum = userNum;
+			for (int i = 0; toolName[i] == NULL; i++)
+			{
+				temp->toolName[i] = toolName[i];
+			}
+			temp->quantity = intInput;
+			temp->cost = doubleInput;
+			temp->next = headListSorted;
+			headListSorted = temp;
+
+			previous->next = temp;
+			temp->next = location;
+		}
+
+		if (location->recordNum > userNum)
+		{
+			temp = new listNode;
+			temp->recordNum = userNum;
+			for (int i = 0; toolName[i] == NULL; i++)
+			{
+				temp->toolName[i] = toolName[i];
+			}
+			temp->quantity = intInput;
+			temp->cost = doubleInput;
+			temp->next = headListSorted;
+			headListSorted = temp;
+
+			previous->next = temp;
+			temp->next = location;
+		}
+	}
+
 }
 
 int main()
@@ -83,6 +158,9 @@ int main()
 	fstream ons;
 
 	ons.open(onFile);
+
+	listSortedHead = 0;
+	
 
 	addRecord(arrSorted, arrUnsorted, listSortedHead, listUnsortedHead, ons);
 
